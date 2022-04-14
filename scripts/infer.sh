5272cbbe59bed0ba0f508510055703ae6787b5aa
MiuGod0126 <68314259+MiuGod0126@users.noreply.github.com>
2022-04-14 12:14:41 +0800

    Update infer.sh

--- scripts/infer.sh
+++ scripts/infer.sh
@@ -1,1 +1,1 @@
-python deploy/inference_python/infer.py --model-dir infer_output  --data-path datasets/tiny_sample.npy
+python deploy/inference_python/infer.py --model-dir infer_output  --data-path test_tipc/lite_data/tiny_sample.npy
